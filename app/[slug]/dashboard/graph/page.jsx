74a86213da82e6fd3a35e8850276c692ece6b309
Saish Mungase <saishmungase@gmail.com>
2025-03-06 00:53:53 +0530

    Graph Added !

---
+++ app/[slug]/dashboard/graph/page.jsx
@@ -0,0 +1,7 @@
+import { AQIGraph } from "@/components/aqi-graph";
+
+export default function Page() {
+    return <div className="p-8 rounded-xl bg-[#ffffffe0]">
+        <AQIGraph />
+    </div>
+}
