a35332371c62766fd2e235ef84c8ed951919e5c8
Atharva Kadam <atharvakadam751@gmail.com>
2025-03-06 11:48:47 +0530

    Step towards Backend and authnetication

---
+++ app/signup/page.jsx
@@ -0,0 +1,21 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/router";
+import { Axios } from "axios";
+
+export default function SignupPage() {
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+        username: "",
+    });
+
+    const onSignup = async () => {
+    }
+
+
+  return (
+    <div></div>
+  )
+}
