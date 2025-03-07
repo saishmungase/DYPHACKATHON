e4d5572f36b2f938f1c852d737ac077ec8c4f024
Saish Mungase <saishmungase@gmail.com>
2025-03-08 03:34:34 +0530

    cityLonLat Added !

---
+++ libs/cityCoords.js
@@ -0,0 +1,9 @@
+
+export const cityLonLat = {
+    pune : {lat : 18.5204, lon : 73.8567},
+    delhi: { lat: 28.6139, lon: 77.2090 },
+    mumbai: { lat: 19.0760, lon: 72.8777 },
+    bangalore: { lat: 12.9716, lon: 77.5946 },
+    newyork: { lat: 40.7128, lon: -74.0060 },
+    london: { lat: 51.5074, lon: -0.1278 },
+}
